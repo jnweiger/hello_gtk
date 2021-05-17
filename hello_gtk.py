e8d233c407533cd6ac0a22a60236b6519fc8549a
Juergen Weigert <jnweiger@gmail.com>
2021-05-18 00:47:03 +0200

    i

---
+++ hello_gtk.py
@@ -0,0 +1,30 @@
+#! /usr/bin/python3
+# FROM: 
+#  - https://www.gtk.org/docs/language-bindings/python/
+#  - https://zetcode.com/python/gtk/
+#  - https://python-gtk-3-tutorial.readthedocs.io/en/latest/layout.html#example
+#
+
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+def on_activate(app):
+    win = Gtk.ApplicationWindow(application=app)
+    hbox = Gtk.HBox(spacing=6)
+    vbox = Gtk.VBox(spacing=6)
+    win.add(hbox)
+    hbox.pack_start(vbox, True, True, 10)
+    btn = Gtk.Button(label="Hello, World!")
+    btn.connect('clicked', lambda x: win.close())
+    btn2 = Gtk.Button(label="Nothing")
+    btn2.connect('clicked', lambda x: print("Nothing ", x))
+    vbox.pack_start(btn, expand=True, fill=True, padding=10)
+    vbox.pack_start(btn2, expand=True, fill=True, padding=10)
+    win.connect("destroy", Gtk.main_quit)
+    win.show_all()
+
+app = Gtk.Application(application_id='org.gtk.Example')
+app.connect('activate', on_activate)
+app.run(None)
+
---
+++ setup.py
@@ -0,0 +1,39 @@
+# FROM https://stackoverflow.com/questions/15734703/use-cx-freeze-to-create-an-msi-that-adds-a-shortcut-to-the-desktop
+# See also:
+# - https://docs.microsoft.com/en-us/windows/win32/msi/property-reference?redirectedfrom=MSDN#System_Folder_Properties
+# - https://docs.microsoft.com/en-us/windows/win32/msi/property-reference#system-folder-properties
+from cx_Freeze import *
+
+# http://msdn.microsoft.com/en-us/library/windows/desktop/aa371847(v=vs.85).aspx
+# FIXME: TARGETDIR probably needs to be replaced with something that actually exists?
+shortcut_table = [
+    ("DesktopShortcut",        # Shortcut
+     "DesktopFolder",          # Directory_     
+     "Hello from Qt5",          # Name
+     "TARGETDIR",              # Component_
+     "[TARGETDIR]helloqt5.exe",# Target
+     None,                     # Arguments
+     None,                     # Description
+     None,                     # Hotkey
+     None,                     # Icon
+     None,                     # IconIndex
+     None,                     # ShowCmd
+     'TARGETDIR'               # WkDir
+     )
+    ]
+
+
+setup(
+    options = {
+        "bdist_msi": {"Shortcut": shortcut_table},
+    },
+    executables = [
+        Executable(
+            "hello_gtk.py",
+            shortcutName="Hello Gtk",
+            shortcutDir="DesktopFolder",        # any of the System Folder Properries
+            )
+        ]
+    )
+
+## Use ALLUSERS=1 on the msiexec command line so that an administrator install adds the desktop shortcut to all users.
