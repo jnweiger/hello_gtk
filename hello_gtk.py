3b87ffe63b6eeed4da17edcc25cf73731a8b1b19
Juergen Weigert <jnweiger@gmail.com>
2023-11-20 15:09:54 +0100

    yes, just a comment

--- hello_gtk.py
+++ hello_gtk.py
@@ -9,6 +9,8 @@ import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
+# kljlkjhlkj lkj 
+
 def on_activate(app):
     win = Gtk.ApplicationWindow(application=app)
     hbox = Gtk.HBox(spacing=6)
